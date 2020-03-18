55542db195b332d35ea4b6c123576000f5ac114b
sram <birdram@gmail.com>
2020-03-18 11:03:00 +0900

    Updates

---
+++ precache-manifest.eae2e3377de630d1a18b2ea7eb216fa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04fe13abb0e6366f61522cd00a72a955",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "3f5307ea1ebd7e5cf7ea",
+    "url": "/movie_app_2020/static/css/main.9f4069f4.chunk.css"
+  },
+  {
+    "revision": "6370162ab7246862677e",
+    "url": "/movie_app_2020/static/js/2.0690ae16.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2020/static/js/2.0690ae16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5307ea1ebd7e5cf7ea",
+    "url": "/movie_app_2020/static/js/main.fcb00b10.chunk.js"
+  },
+  {
+    "revision": "5b9de689d76c950937df",
+    "url": "/movie_app_2020/static/js/runtime-main.db2dcdc9.js"
+  }
+]);
